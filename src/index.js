54cb9f850c38e5d5924264d9b5c362acd34283eb
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2023-09-18 04:51:49 +0300

    settings

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/">
+        <BrowserRouter basename="/wander_wheels">
           <App />
         </BrowserRouter>
       </PersistGate>
